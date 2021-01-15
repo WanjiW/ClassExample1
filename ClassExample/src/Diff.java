2951d9bb2fab88c1579d3f7899188ea3d665ca7b
Wanjiru Wangondu <w.wangondu@alustudent.com>
2021-01-15 11:57:07 +0300

    W commit

---
+++ ClassExample/src/Diff.java
@@ -0,0 +1,12 @@
+public class Diff {
+
+	public static void main(String[] args) {
+				
+				int a = 7, b = 3, difference;
+				difference = a - b;
+				
+		        System.out.println("The difference is: "+difference);
+			}
+
+
+}
